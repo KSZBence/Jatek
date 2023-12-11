80737a87cf37f300d86e77095880c0b80e7560ab
KSZBence <kunszekelybence@gmail.com>
2023-12-11 13:19:37 +0100

    jatek es karakter

---
+++ src/jatek/Karakter.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jatek;
+
+/**
+ *
+ * @author kunszekely.bence
+ */
+public class Karakter {
+    public static void main(String[] args) {
+        Karakter k1 = new Karakter();
+    }
+}
